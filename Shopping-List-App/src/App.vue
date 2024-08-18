f094877f89f7a1f5fc3367ffbf61f25bad567bf7
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-08-18 14:57:12 -0300

    feature: Explorando eventos com v-on.

--- Shopping-List-App/src/App.vue
+++ Shopping-List-App/src/App.vue
@@ -12,15 +12,17 @@ const newItemHighPriority = ref(false)
 </script>
 
 <template>
-  
-  <input type="text" v-model="newItem" placeholder="Add a New Item">
-  Priority:
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority" value=true>
-    High Priority
-  </label>
-
   <h1>{{ header }}</h1>
+
+  <form class="add-item-form" @submit.prevent="items.push({id: items.length + 1, label: newItem})">
+    <input type="text" v-model="newItem" placeholder="Add a New Item">
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" value=true>
+      High Priority
+    </label>
+    <button class="btn btn-primary">Save Item</button>
+  </form>
+
   <ul>
     <li v-for="{id, label} in items" :key="id">{{ label }}</li>
   </ul>
