fa8f961129af23ad360dbe9c16f7da7ef181630a
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-08-18 14:36:15 -0300

    feature: Explorando o v-model.

--- Shopping-List-App/src/App.vue
+++ Shopping-List-App/src/App.vue
@@ -7,10 +7,19 @@ const items = ref([
   {id: 2, label: '2 board games'},
   {id: 3, label: '20 cups'}
 ])
+const newItem = ref('')
+const newItemHighPriority = ref(false)
 </script>
 
 <template>
   
+  <input type="text" v-model="newItem" placeholder="Add a New Item">
+  Priority:
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority" value=true>
+    High Priority
+  </label>
+
   <h1>{{ header }}</h1>
   <ul>
     <li v-for="{id, label} in items" :key="id">{{ label }}</li>
